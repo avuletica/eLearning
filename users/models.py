ce532b7b663e1f2c65d8f5280d6b1a793526c539
Bcoolie <vuletic23@gmail.com>
2016-04-02 17:01:55 +0200

    Add general model for LogIn

--- users/models.py
+++ users/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class LogIn(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=50,)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):  # Python 3.3 is __str__
+        return self.email
