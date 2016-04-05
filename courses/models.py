44d4779b40bb1d00293849585b34b0bf6717fe66
Ante M Marin <ante.m.marin@gmail.com>
2016-04-05 19:07:24 +0200

    create TeachingBlock model with link and text field

--- courses/models.py
+++ courses/models.py
@@ -7,14 +7,17 @@ from users.models import User
 class Course(models.Model):
 	course_name = models.CharField(max_length=50)
 	created_date = models.DateTimeField(auto_now_add=True)
-	professor = models.ForeignKey(User) # verify
+	professor_fk = models.ForeignKey(User) # verify
 	free_course = models.BooleanField(default='True') #free or not free
 
 
 class Chapter(models.Model):
 	chapter_name = models.CharField(max_length=50)
 	created_date = models.DateTimeField(auto_now_add=True)
-	course = models.ForeignKey(Course)
+	course_fk = models.ForeignKey(Course)
+
+class TeachingBlock(models.Model):
 	link = models.URLField(max_length=200, default='Insert your YouTube video link here')
 	chapter_description = models.TextField(max_length=2048, default='Write your course description here')
+	chapter_fk = models.ForeignKey(Chapter)	
 	
