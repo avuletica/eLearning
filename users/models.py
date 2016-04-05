5452c34902931e80345a590d985a0b32fcfbe128
Ante M Marin <ante.m.marin@gmail.com>
2016-04-05 13:31:29 +0200

    Remove LogIn model, add generic User model

--- users/models.py
+++ users/models.py
@@ -5,24 +5,14 @@ from django.db import models
 
 
 # Create your models here.
-class LogIn(models.Model): 	
-    email = models.EmailField()
-    password = models.CharField(max_length=50,)
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
-    def __unicode__(self):
-        return self.email
-
-
-class CreateStudent(models.Model):
+class User(models.Model):
 	first_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
 	email = models.EmailField()
 	password = models.CharField(max_length=30)
 	#timestamp - when user is created
 	timestamp = models.DateTimeField(auto_now_add=True)
+	privilege = models.BooleanField(default=False)
 
-class Professor(CreateStudent):
-	pass
 
