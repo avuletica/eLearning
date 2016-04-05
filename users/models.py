082452bf1d0431e955525b6ee60fa480ff14847e
Bcoolie <vuletic23@gmail.com>
2016-04-05 21:20:30 +0200

    Remove old LogIn model

--- users/models.py
+++ users/models.py
@@ -4,11 +4,3 @@ from django.db import models
 
 
 # Create your models here.
-class LogIn(models.Model):
-    email = models.EmailField()
-    password = models.CharField(max_length=50,)
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-
-    def __unicode__(self):  # Python 3.3 is __str__
-        return self.email
