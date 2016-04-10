9995ee55cb2d0295273ada9f8181b9511ea54131
Bcoolie <vuletic23@gmail.com>
2016-04-10 19:30:21 +0200

    Modify course model to include course link

--- courses/models.py
+++ courses/models.py
@@ -1,26 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from users.models import UserProxy
+from users.models import User
+
 
 # Create your models here.
 class Course(models.Model):
-	course_name = models.CharField(max_length=50)
-	created_date = models.DateTimeField(auto_now_add=True)
-	user_fk = models.ManyToManyField(UserProxy) # verify
-	free_course = models.BooleanField(default='True') #free or not free
+    course_name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    course_link = models.CharField(max_length=50)
+    user_fk = models.ManyToManyField(User)
 
 
 class Chapter(models.Model):
-	chapter_name = models.CharField(max_length=50)
-	created_date = models.DateTimeField(auto_now_add=True)
-	course_fk = models.ForeignKey(Course)
-	
+    chapter_name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    course_fk = models.ForeignKey(Course)
+
+
 class TextBlock(models.Model):
-	chapter_description = models.TextField(max_length=2048, default='')
-	text_area_fk = models.ForeignKey(Chapter)
+    chapter_description = models.TextField(max_length=2048, default='')
+    text_area_fk = models.ForeignKey(Chapter)
+
 
 class YTLink(models.Model):
-	link = models.URLField(max_length=200, default='')
-	youtube_fk = models.ForeignKey(Chapter)	
-	
+    link = models.URLField(max_length=200, default='')
+    youtube_fk = models.ForeignKey(Chapter)
