fb7b29b74e1b3a32014956ae8c3a55f12a1d1c96
Bcoolie <vuletic23@gmail.com>
2016-04-12 23:58:19 +0200

    Implement form for adding and deleting new users at sysadmin dashboard

--- courses/forms.py
+++ courses/forms.py
@@ -1,8 +1,21 @@
 from django import forms
 from .models import Course
+from users.models import *
 
 
 class AddCourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['course_name']
+
+
+class AddUser(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+
+class DeleteUser(forms.ModelForm):
+    class Meta:
+        model = DeleteUser
+        fields = ['username']
