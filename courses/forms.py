75b5daa51798f7ef85a50061dc0a5b6b1995c53e
Bcoolie <vuletic23@gmail.com>
2016-04-13 00:58:55 +0200

    Remove old forms

--- courses/forms.py
+++ courses/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 from .models import Course
-from users.models import *
 
 
 class AddCourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['course_name']
-
-
