aca332119790d69dd09c880f2d7910168bbf10bf
anabolanca <abolanca@fesb.hr>
2016-04-12 19:24:43 +0200

    add profile

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from source import settings
+from courses.views import course
+from courses.forms import AddCourseForm
 
 
 def home(request):
@@ -9,3 +11,24 @@ def home(request):
     }
 
     return render(request, "home.html", context)
+
+
+def profile(request):
+    title = 'Profile'
+    form = AddCourseForm(request.POST or None)
+
+    context = {
+        "title": title,
+        "form": form,
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.course_link = '/courses/' + instance.course_name
+        instance.save()
+        return course(request, course_name=instance.course_name)
+
+    if request.user.is_authenticated():
+        return render(request, "user_profile.html", context)
+    else:
+        return redirect(settings.LOGIN_URL)
