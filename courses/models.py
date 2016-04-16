d13f572407b6ec03b5d21b74a9322e99203722d0
anabolanca <abolanca@fesb.hr>
2016-04-16 19:49:46 +0200

    modify models

--- courses/models.py
+++ courses/models.py
@@ -21,28 +21,21 @@ class Course(models.Model):
 class Chapter(models.Model):
     chapter_name = models.CharField(max_length=50)
     chapter_created_date = models.DateTimeField(auto_now_add=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=1)
 
     def __unicode__(self):
         return self.chapter_name
 
     def get_absolute_url(self):
-        return reverse("course", kwargs={"chapter_name": self.chapter_name})
+        return reverse("chapter", kwargs={"course_name": self.course,
+                                          "chapter_name": self.chapter_name})
 
 
 class TextBlock(models.Model):
     chapter_description = models.TextField()
-    text_block_fk = models.ForeignKey(Chapter)
+    text_block_fk = models.ForeignKey(Chapter, default=1)
 
 
 class YTLink(models.Model):
     link = models.URLField(unique=True, max_length=200, default='')
-    yt_link_fk = models.ForeignKey(Chapter)
-
-
-class DeleteCourse(models.Model):
-    course_name = models.CharField(max_length=50)
-
-
-class DeleteChapter(models.Model):
-    chapter_name = models.CharField(max_length=50)
+    yt_link_fk = models.ForeignKey(Chapter, default=1)
