04f938bda66cb3fd534e47f2a22bbeee7c9a9994
Bcoolie <vuletic23@gmail.com>
2016-04-18 00:40:00 +0200

    Extend user model

--- users/models.py
+++ users/models.py
@@ -1,10 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 
 
-# Create your models here.
-class UserProxy(User):
-    class Meta:
-        proxy = True
+class UserProfile(AbstractUser):
+    is_professor = models.BooleanField(default=False)
+    is_site_admin = models.BooleanField(default=False)
