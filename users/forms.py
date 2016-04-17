af53599c7f2abb563c9f55392afe9e22e14b56eb
Bcoolie <vuletic23@gmail.com>
2016-04-18 00:39:33 +0200

    Change user model User -> UserProfile

--- users/forms.py
+++ users/forms.py
@@ -5,17 +5,17 @@ from crispy_forms.helper import FormHelper
 
 class AddUser(forms.ModelForm):
     class Meta:
-        model = User
+        model = UserProfile
         widgets = {
             'password': forms.PasswordInput(),
         }
-        fields = ['username', 'password', 'email', 'is_staff', 'is_superuser']
+        fields = ['username', 'password', 'email', 'is_professor', 'is_site_admin']
 
 
 class EditUser(forms.ModelForm):
     class Meta:
-        model = User
+        model = UserProfile
         widgets = {
             'password': forms.PasswordInput(),
         }
-        fields = ['username', 'password', 'email', 'is_staff', 'is_superuser']
+        fields = ['username', 'password', 'email', 'is_professor', 'is_site_admin']
