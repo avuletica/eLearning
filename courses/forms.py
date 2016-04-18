ff5051d768e2af465e5f9dc1e00fae8e193bcbd6
Bcoolie <vuletic23@gmail.com>
2016-04-18 22:46:38 +0200

    Add validation on form

--- courses/forms.py
+++ courses/forms.py
@@ -7,8 +7,24 @@ class AddCourseForm(forms.ModelForm):
         model = Course
         fields = ['course_name']
 
+    def clean_course_name(self):
+        course_name = self.cleaned_data.get('course_name')
+
+        if not course_name.isalpha():
+            raise forms.ValidationError("Please make sure all characters are alphabetic")
+
+        return course_name
+
 
 class AddChapterForm(forms.ModelForm):
     class Meta:
         model = Chapter
         fields = ['chapter_name']
+
+    def clean_chapter_name(self):
+        chapter_name = self.cleaned_data.get('chapter_name')
+
+        if not chapter_name.isalpha():
+            raise forms.ValidationError("Please make sure all characters are alphabetic")
+
+        return chapter_name
