13dc629eea6d2cedb8c0d51ddf9ddf786963fb77
Bcoolie <vuletic23@gmail.com>
2016-04-19 19:34:59 +0200

    Modify regex

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,8 @@ from . import views as course_views
 
 urlpatterns = [
     url(r'^$', course_views.courses, name='courses'),
-    url(r'^(?P<course_name>\w+)/$', course_views.course, name='course'),
-    url(r'^(?P<course_name>\w+)/delete/$', course_views.delete_course, name='delete'),
-    url(r'^(?P<course_name>\w+)/(?P<chapter_name>\w+)/$', course_views.chapter, name='chapter'),
-    url(r'^(?P<course_name>\w+)/(?P<chapter_id>\d+)/delete/$', course_views.delete_chapter, name='delete_chapter'),
+    url(r'^(?P<course_name>[\w ]+)/$', course_views.course, name='course'),
+    url(r'^(?P<course_name>[\w ]+)/delete/$', course_views.delete_course, name='delete'),
+    url(r'^(?P<course_name>[\w ]+)/(?P<chapter_name>[\w ]+)/$', course_views.chapter, name='chapter'),
+    url(r'^(?P<course_name>[\w ]+)/(?P<chapter_id>\[\d ]+)/delete/$', course_views.delete_chapter, name='delete_chapter'),
 ]
