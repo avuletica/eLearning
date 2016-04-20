df4f1cb79a64e5793f9059ff2c12c388fc58005d
Bcoolie <vuletic23@gmail.com>
2016-04-20 15:01:54 +0200

    Add constructor to form

--- users/forms.py
+++ users/forms.py
@@ -18,3 +18,11 @@ class EditUser(forms.ModelForm):
             'password': forms.PasswordInput(),
         }
         fields = ['username', 'password', 'email', 'is_professor', 'is_site_admin']
+
+    # Don't want to modify blank setting inside models (doing so will break normal validation in admin site)
+    # The redefined constructor won't harm any functionality.
+    def __init__(self, *args, **kwargs):
+        super(EditUser, self).__init__(*args, **kwargs)
+
+        for key in self.fields:
+            self.fields[key].required = False
