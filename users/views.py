37649d949b341c78cfe8e5029b899f9a4ee57729
Bcoolie <vuletic23@gmail.com>
2016-04-20 23:50:07 +0200

    Change render path

--- users/views.py
+++ users/views.py
@@ -50,12 +50,12 @@ def profile(request):
         return redirect(instance.get_absolute_url())
 
     if request.user.is_professor:
-        return render(request, "professor_dashboard.html", context)
+        return render(request, "users/professor_dashboard.html", context)
 
     elif request.user.is_site_admin:
-        return render(request, "sysadmin_dashboard.html", context)
+        return render(request, "users/sysadmin_dashboard.html", context)
     else:
-        return render(request, "student_dashboard.html", context)
+        return render(request, "users/student_dashboard.html", context)
 
 
 @user_passes_test(lambda user: user.is_site_admin)
@@ -63,7 +63,7 @@ def update_user(request, username):
     user = UserProfile.objects.get(username=username)
     data_dict = {'username': user.username, 'email': user.email}
     update_user_form = EditUser(initial=data_dict, instance=user)
-    title = 'Edit user'
+    title = 'Edit users'
     context = {
         "title": title,
         "update_user_form": update_user_form,
@@ -83,7 +83,7 @@ def update_user(request, username):
 
             return redirect(reverse('profile'))
 
-    return render(request, "edit_user.html", context)
+    return render(request, "users/edit_user.html", context)
 
 
 @user_passes_test(lambda user: user.is_site_admin)
