05791ce718a8b0636cbd7e0cace15e020cf339bc
Ante M Marin <ante.m.marin@gmail.com>
2016-04-26 20:15:33 +0200

    changed urls

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from . import views as course_views
+from users import views as user_views
 
 urlpatterns = [
     url(r'^$', course_views.courses, name='courses'),
 
-    url(r'^(?P<course_name>[\w ]+)/$', course_views.course, name='course'),
+    url(r'^(?P<course_name>[\w ]+)/$', user_views.course_homepage, name='course_homepage'),
+    url(r'^(?P<course_name>[\w ]+)/(?P<chapter_name>[\w ]+)/$', user_views.student_course,
+        name='student_courses'),
    
 ]
--- users/urls.py
+++ users/urls.py
@@ -33,9 +33,7 @@ urlpatterns = [
 
 
     url(r'^student/$', user_views.student, name='student'),
-    url(r'student/(?P<course_name>[\w ]+)/$', user_views.course_homepage, name='course_homepage'),
-    url(r'^student/(?P<course_name>[\w ]+)/(?P<chapter_name>[\w ]+)/$', user_views.student_course,
-        name='student_courses'),
+
 
     url(r'^edit/(?P<username>[\w ]+)/$', user_views.update_user, name='update_user'),
     url(r'^delete/(?P<username>[\w ]+)/$', user_views.delete_user, name='delete_user'),
