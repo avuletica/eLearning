a70256dc531284a6a0f8fec96f8d79bef3aca632
Bcoolie <vuletic23@gmail.com>
2016-04-30 21:06:47 +0200

    Improve structure & fix forum script

--- users/static_in_users/static_files/js/forum.js
+++ users/static_in_users/static_files/js/forum.js
@@ -2,31 +2,37 @@
  * Created by ante on 4/28/16.
  */
 $(document).ready(function () {
-    var topic = $(".add-btn");
-    var returnLink = $(".return-link");
-    var block = $(".add-topic");
-    topic.on("click", handleAddTopicClick);
-    returnLink.on("click", handleAddTopicClick);
-
-    function handleAddTopicClick() {
-        block.toggle();
-        topic.toggle();
-        returnLink.toggle();
-
-        $('html,body').animate({
-            scrollTop: $(window).scrollTop() + block.height()/2
-        })
-    }
-
     // Bind the event handler to the "submit" JavaScript event
     $('.add-topic form').submit(function () {
-        var check = $.trim($('#id_topic_message').val());
-        var check2 = $.trim($('#id_subject').val());
+        var $subject = $.trim($('#id_subject').val());
+        var formData = tinyMCE.activeEditor.getContent({format: 'raw'});
+
+        // Strip html tags from formData
+        var regex = /(<([^>]+)>)/ig;
+        var editorContent = formData.replace(regex, "");
+
 
         // Check if empty of not
-        if (check === '' || check2 === '') {
+        if ($subject === '' || editorContent === '') {
             alert("Can't submit empty fields");
             return false;
         }
     });
+
+    var $topic = $(".add-btn");
+    var $returnLink = $(".return-link");
+    var $block = $(".add-topic");
+    $topic.on("click", handleAddTopicClick);
+    $returnLink.on("click", handleAddTopicClick);
+
+    function handleAddTopicClick() {
+        $block.toggle();
+        $topic.toggle();
+        $returnLink.toggle();
+
+        $('html,body').animate({
+            scrollTop: $(window).scrollTop() + block.height() / 2
+        })
+    }
+
 });
