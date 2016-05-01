f27765703e82f55380cb8caf2f89a5a96a513920
Bcoolie <vuletic23@gmail.com>
2016-05-01 16:06:04 +0200

    Implement unit tests

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
-# Create your tests here.
+from users.models import UserProfile
+from forum.views import forum
+
+
+class TestCalls(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = UserProfile.objects.create_user(
+            username='Hodor', email='test@gmail.com', password='top_secret')
+
+    def test_call_view_denies_anonymous(self):
+        response = self.client.get('forum', follow=True)
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('topic', follow=True)
+        self.assertEqual(response.status_code, 404)
+
+    # Test forum view after login
+    def test_call_view_loads(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('forum')
+        request.user = self.user
+        response = forum(request)
+        self.assertEqual(response.status_code, 200)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.hashers import make_password
 
-# Create your tests here.
+from .models import UserProfile
+from courses.views import courses
+
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = UserProfile.objects.create_user(
+            username='Hodor', email='test@gmail.com', password='top_secret')
+
+    def test_user_can_register(self):
+        UserProfile.objects.create(username="user_name_sample", email='t@t.com', password=make_password('abcd'))
+        user = UserProfile.objects.get(username="user_name_sample")
+        self.assertEqual(user.username, 'user_name_sample')
+
+    def test_user_can_login(self):
+        request = self.factory.get('courses')
+        request.user = self.user
+        response = courses(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_call_view_denies_anonymous(self):
+        response = self.client.get('profile', follow=True)
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('courses', follow=True)
+        self.assertEqual(response.status_code, 404)
